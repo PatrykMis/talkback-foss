46001476cda6e4858d014ce2ccce30a229bd80fa
Patryk Mis <patryk.mis@mykolab.com>
2022-05-26 07:15:54 +0200

    Workarround to fix bug in Gradle 7.4.x

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,10 +9,14 @@ repositories {
     google()
 }
 
-tasks.withType<KotlinCompile>().configureEach {
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        apiVersion = "1.6"
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = "1.8"
     }
 }
 
